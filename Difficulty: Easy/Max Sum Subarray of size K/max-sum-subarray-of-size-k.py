828cbe973fb534c2a9c68a3a9ca71548cfdc6c5f
Anakhi Dutta <anakhidutta@gmail.com>
2026-05-09 12:34:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubarraySum(self, arr, k):
+        n = len(arr)
+
+        # Sum of first window
+        window_sum = sum(arr[:k])
+        max_sum = window_sum
+
+        # Slide the window
+        for i in range(k, n):
+            window_sum += arr[i] - arr[i - k]
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum
+
+        
